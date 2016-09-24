951d3081472f942118472baa3e9893f05e1f5bae
Nik Ambukovski <lazokin@hotmail.com>
2016-09-24 20:26:33 +1000

    Class Variables and Instance Variables

--- app/src/main/java/com/lazokin/tipcalculator/MainActivity.java
+++ app/src/main/java/com/lazokin/tipcalculator/MainActivity.java
@@ -2,8 +2,23 @@ package com.lazokin.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final NumberFormat currencyFormat =
+            NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat =
+            NumberFormat.getPercentInstance();
+
+    private double billAmount = 0.0;
+    private double percent = 0.15;
+    private TextView amountTextView;
+    private TextView percentTextView;
+    private TextView tipTextView;
+    private TextView totalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
